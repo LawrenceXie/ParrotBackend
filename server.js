dff1897c135885a6670c18163a162071ba8f46f7
Lawrence Xie <lawrence.xie90@gmail.com>
2019-04-16 22:33:34 -0400

    changed for submodule pathing

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "ParrotFrontend/build")));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,6 +16,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const configureRoutes = require("./routes");
 
 configureRoutes(app);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/ParrotFrontend/build/index.html"));
+});
 
 app.listen(8000, error => {
   if (error) throw error;
